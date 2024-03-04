ea23f0bfdf1fa74f62580623d4a9b9ecdb454b5c
Krishna Garcha <148072306+krish-134@users.noreply.github.com>
2024-03-03 19:51:55 -0800

    Update gitprofile.config.ts - updated resume

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -68,7 +68,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1apgasxBqjykmPVLxjcqgX2BroKzOo6oS/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1PDawDi2f1Q_dvH15xfR5RvRmALNinWdJ/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Racket - ISL',
